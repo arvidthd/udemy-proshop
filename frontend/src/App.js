d4008f6f782d890846442381d44aefb221d30669
arvidthd <arvid.thd@gmail.com>
2023-06-16 16:15:31 +0700

    Added routing component for client side rendering

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,8 @@ import { Container } from 'react-bootstrap';
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import HomeScreen from './screens/HomeScreen';
+// import HomeScreen from './screens/HomeScreen';
+import { Outlet } from 'react-router-dom';
 
 export const App = () => {
   return (
@@ -11,7 +12,7 @@ export const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Outlet />
         </Container>
       </main>
       <Footer />
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { Navbar, Nav, Container } from 'react-bootstrap'
 import { FaShoppingCart, FaUser } from 'react-icons/fa'
 
+//EXP : because there are specific element for Navbar.Brand we need to use Link Container to make it client rendered link
+import { LinkContainer } from 'react-router-bootstrap'
+
 import logo from '../assets/logo.png'
 
 const Header = () => {
@@ -9,19 +12,25 @@ const Header = () => {
     <header>
         <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
             <Container>
-                <Navbar.Brand href="/">
-                    <img src={logo} alt="VidShop Logo" />
-                    ProShop
-                </Navbar.Brand>
+                <LinkContainer to="/">
+                    <Navbar.Brand>
+                        <img src={logo} alt="VidShop Logo" />
+                        ProShop
+                    </Navbar.Brand>
+                </LinkContainer>
                 <Navbar.Toggle aria-controls='basic-navbar-nav' />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className='ms-auto'>
-                        <Nav.Link href="/cart">
-                            <FaShoppingCart /> Cart
-                        </Nav.Link>
-                        <Nav.Link href="/login">
-                            <FaUser /> SignIn
-                        </Nav.Link>
+                        <LinkContainer to="/cart">
+                            <Nav.Link>
+                                <FaShoppingCart /> Cart
+                            </Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/login">
+                            <Nav.Link >
+                                <FaUser /> SignIn
+                            </Nav.Link>
+                        </LinkContainer>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
